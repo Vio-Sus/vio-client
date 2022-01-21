6ccf7b2d935650d97d9b87ca62d7e671e330a306
Sara Wasim <sarawasim@hotmail.com>
2022-01-21 15:54:14 -0800

    create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [whatever, setWhatever] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((result) => result.data)
+      .then((data) => setWhatever(data.message));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{whatever}</h1>
     </div>
   );
 }
