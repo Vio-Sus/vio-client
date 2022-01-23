4c0638695734b10b2b55c9fe87d7a7a9802762d7
Sara Wasim <sarawasim@hotmail.com>
2022-01-22 20:40:33 -0800

    added a test input form

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,14 @@
 import { useEffect, useState } from 'react';
 import { getSources } from '../network';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const SelectDate = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+  );
+};
 
 export default function Form() {
   const [sources, setSources] = useState([]);
@@ -13,11 +22,37 @@ export default function Form() {
   return (
     <>
       <form>
-        {sources.map((source,key) => (
-          <p key={key}>{source.name}</p>
-        ))}
-
-        <input type="submit"></input>
+        <label>
+          {' '}
+          Select a Date:
+          <SelectDate />
+        </label>
+        <br />
+        <br />
+        <label>
+          Choose Source:
+          <select>
+            {sources.map((source, key) => (
+              <option key={key}>{source.name}</option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <br />
+        <label>
+          Select Item:
+          <select>
+            <option>Item 1</option>
+            <option>Item 2</option>
+          </select>
+        </label>
+        <label>
+          Weight:
+          <input type="text" />
+        </label>
+        <br />
+        <br />
+        <input type="submit" />
       </form>
     </>
   );
