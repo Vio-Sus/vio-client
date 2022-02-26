8f8511e1feec9704b08e847df2078bc8d57e3d74
Megan Kuo <megan.kuo@live.com>
2022-02-26 08:55:38 -0800

    AddSourceForm component complete

--- src/components/Source/AddSourceForm.jsx
+++ src/components/Source/AddSourceForm.jsx
@@ -1,61 +1,81 @@
 import { useState } from 'react';
 import { postSource } from '../../network';
-// import { handleValidation } from '../../validation';
 
 export default function AddSourceForm({ items, sources }) {
-  const [formValues, setFormValues] = useState({});
-  const [errorMsgs, setErrorMsgs] = useState([]);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
-  let handleFormValues = (e) => {
-
+  const handleChange = (e) => {
+    let inputName = e.target.name;
+    console.log('inputName: ', inputName, 'inputValue: ', e.target.value);
+    switch (inputName) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'address':
+        setAddress(e.target.value);
+        break;
+      case 'phoneNumber':
+        setPhoneNumber(e.target.value);
+        break;
+      default:
+        return;
+    }
   };
 
-  let handleSubmit = async (event) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let form = document.getElementById('new-source-form');
+    let formContent = {
+      name,
+      address,
+      phoneNumber,
+    };
+    try {
+      console.log('sending form...', formContent);
+      let res = await postSource(formContent);
+      console.log(res);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const handleCancel = () => {
+    // TODO: closes the form div
   };
 
   return (
     <>
       <h2>Add a new source</h2>
-      <form onSubmit={handleSubmit} id="input-form" noValidate>
+      <form onSubmit={handleSubmit} id="new-source-form" noValidate>
         <label>Name:</label>
         <input
           name="name"
           type="text"
-          onChange={(e) => handleFormValues(e)}
+          onChange={(e) => handleChange(e)}
         ></input>
         <br />
         <label>Address:</label>
         <input
-          name="name"
+          name="address"
           type="text"
-          onChange={(e) => handleFormValues(e)}
+          onChange={(e) => handleChange(e)}
         ></input>
         <br />
-        {entryWeights.map((element, index) => (
-          <div className="form-inline" key={element.id}>
-            <label>Item</label>
-            <input></input>
-            <label>Weight</label>
-            <input
-              type="number"
-              name="weight"
-              onChange={(e) => {
-                element.weight = Number(e.target.value);
-              }}
-            />
-          </div>
-        ))}
-        <div className="error-messages">
-          {errorMsgs.map((msg, key) => (
-            <span key={key}>{msg}</span>
-          ))}
-        </div>
+        <label>Contact Number:</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          onChange={(e) => handleChange(e)}
+        />
 
         <div className="button-section">
           <button className="button submit" type="submit">
             Save Source
           </button>
+          <button onClick={handleCancel}>Cancel</button>
         </div>
       </form>
     </>
