46523e7ac1c5d8b6e70ea9dc1d1b8fbdba156cb0
Jo Jo Lam <jojilam@gmail.com>
2022-03-04 17:07:10 -0800

    added selectSource function to filter view data by source

--- src/components/Entry/EntriesList.jsx
+++ src/components/Entry/EntriesList.jsx
@@ -3,6 +3,7 @@ import { getListOfEntries } from '../../network';
 
 export default function EntriesList({ selectEntry, sources }) {
   const [entries, setEntries] = useState([]);
+  const [filteredEntries, setFilteredEntries] = useState([]);
   // useEffect(() => {
   //   getListOfEntries().then((result) => {
   //     console.log(result);
@@ -14,22 +15,40 @@ export default function EntriesList({ selectEntry, sources }) {
       try {
         let [entries] = await Promise.all([getListOfEntries()]); // returns new promise with all data
         setEntries(entries || []);
+        setFilteredEntries(entries || []);
         console.log({ sources });
       } catch {}
     })();
   }, []);
 
+  const selectSource = (sourceId) => {
+    if (sourceId == 'all') {
+      setFilteredEntries(entries);
+    } else {
+      let filtered = entries.filter((entry) => {
+        if (entry['source_id'] == sourceId) {
+          return entry;
+        }
+      });
+      setFilteredEntries(filtered);
+    }
+  };
+
   return (
     <>
+      Filter by source:
       <select>
-        <option hidden>Filter by Source</option>
+        <option onClick={() => selectSource('all')}>All</option>
         {sources.map((source, key) => (
-          <option key={key} value={source.source_id}>
+          <option
+            key={key}
+            value={source.source_id}
+            onClick={() => selectSource(source.source_id)}
+          >
             {source.name}
           </option>
         ))}
       </select>
-
       <table>
         <thead>
           <tr>
@@ -40,7 +59,7 @@ export default function EntriesList({ selectEntry, sources }) {
           </tr>
         </thead>
         <tbody>
-          {entries.map((entry, index) => (
+          {filteredEntries.map((entry, index) => (
             <tr key={index}>
               <td> {entry.entry_date} </td>
               <td> {entry.source_name}</td>
