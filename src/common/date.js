78153fe77879717239261184d6cbe11da22b55f7
Octavio Villena <octaviovillena@hotmail.com>
2022-03-11 18:02:11 -0800

    added stuff to date.js

--- src/common/date.js
+++ src/common/date.js
@@ -4,3 +4,18 @@ export function dateToYMD(date) {
   let dd = date.getDate().toString().padStart(2, '0');
   return `${yyyy}-${mm}-${dd}`;
 }
+
+export function makeToday() {
+  const todayObj = new Date(new Date().toString());
+  const todayDate = dateToYMD(todayObj);
+  return todayDate;
+}
+
+export function makeDefaultStartDate(daysPassed) {
+  const todayObj = new Date(new Date().toString());
+  const todayMinus100 = new Date(
+    new Date().setDate(todayObj.getDate() - daysPassed)
+  );
+  const defaultStartDate = dateToYMD(todayMinus100);
+  return defaultStartDate;
+}
