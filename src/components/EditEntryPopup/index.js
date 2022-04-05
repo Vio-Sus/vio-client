bcb3c13de6e0866be59f288dce1e7b693c23c20d
Catherine Lam <catherinelamyw@gmail.com>
2022-04-05 16:17:18 -0700

    fixed dropdown and input sizes

--- src/components/EditEntryPopup/index.js
+++ src/components/EditEntryPopup/index.js
@@ -1,99 +1,126 @@
-
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import React from 'react';
 
 import Button from '../Button/index';
-import DropDownOptions from '../DropDownOptions';
-import TextInputBox from '../TextInputBox';
 
 import IconButton from '@mui/material/IconButton';
+import CancelIcon from '@mui/icons-material/Cancel';
 import Delete from '@mui/icons-material/Delete';
 import AddCircle from '@mui/icons-material/AddCircle';
 
 const Cont = styled.div`
-    max-width : 400px;
-    min-height : 200px;
     background-color: #F9F9F9;
     border: #ACACAC;
+    margin: 20px;
+    padding: 20px;
     border-radius: 7px;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 20px;
-    padding: 10px;
     border: 1px solid darkgray;
+    max-width: 450px;
 `;
 
-const Header= styled.text`
- font-size: 15px;
- display: flex;
- justify-content: center;
- align-items: center;
-text-align: center;
- padding:25px;
-`;
-
-const List = styled.text`
-color: black;
-display: flex;
-justify-content: flex-start;
-flex-direction: column;
-font-size: 12px;
-margin-left: 35px;
-margin-right: 35px;
+const Header = styled.text`
+    font-size: 15px;
 `;
 
 const RowCont = styled.div`
-    display:flex;
-`;
-
-const Li = styled.text`
-color: black;
-`;
-
-const Buttons = styled.div`
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    margin-top: 20px;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    gap: 10px 0;
 `;
 
-const EditEntryPopup = ({
-   date="03/05/2022",
-   source="VIO Coffee Shop",
-   materials= "Coffee cups, Straws",
-   processor = "processor A"
-}) => {
+const MarginRight = styled.div`
+    width: 35px;
+`;
+
+const SubCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 5px 0;
+    gap: 5px;
+`;
+
+const Dropdown = styled.select`
+    width: 125px;
+`;
+    
+const GenericInput = styled.input`
+    max-width: 117px;
+    min-width: 117px;
+`;
+
+const GenericLabel = styled.label`
+    font-size: 12px;
+`;
+
+const EditEntryPopup = ({}) => {
     return (
         <Cont>
-            <Header>Edit Entry</Header>
             <RowCont>
-                <DropDownOptions/>
-                <DropDownOptions/>
-                <DropDownOptions/>
-            </RowCont>
-            <RowCont>
-                <TextInputBox text="" defaultvalue="Coffee Grinds"/>
-                <TextInputBox text="" defaultvalue="100.22 kg"/>
-                <DropDownOptions/>
-                <IconButton  sx={{ "&:hover": { color: "#60C993" } }}>
-                    <Delete/>
+                <Header>Edit Entry</Header>
+                <IconButton sx={{ "&:hover": { color: "#F86E6E" } }}>
+                    <CancelIcon/>
                 </IconButton>
             </RowCont>
             <RowCont>
-                <TextInputBox text="" defaultvalue="Coffee Grinds"/>
-                <TextInputBox text="" defaultvalue="100.22 kg"/>
-                <DropDownOptions/>
-                <IconButton sx={{ "&:hover": { color: "#60C993" } }}>
-                    <Delete/>
+                <SubCont>
+                    <GenericLabel for="EditEntrySubAccount">Sub-account</GenericLabel>
+                    <Dropdown name="Sub-account" id="EditEntrySubAccount">
+                        <option>VIO Coffee Shop</option>
+                    </Dropdown>
+                </SubCont>
+                <SubCont>
+                    <GenericLabel for="EditEntryDate">Date</GenericLabel>
+                    <GenericInput type="date" name="Date" id="EditEntryDate"/>
+                </SubCont>
+                <SubCont>
+                    <GenericLabel for="EditEntryStatus">Status</GenericLabel>
+                    <Dropdown name="Status" id="EditEntryStatus">
+                        <option>Pending Pickup</option>
+                    </Dropdown>
+                </SubCont>
+                <MarginRight/>
+            </RowCont>
+            <RowCont>
+                <GenericInput text="" placeholder="Coffee Grinds"/>
+                <GenericInput text="" placeholder="100.22 kg"/>
+                <Dropdown>
+                    <option>N. Van Processor</option>
+                </Dropdown>
+                <IconButton sx={{ "&:hover": { color: "#F86E6E" } }}>
+                    <Delete fontSize="small"/>
+                </IconButton>
+            </RowCont>
+            <RowCont>
+                <GenericInput text="" placeholder="Coffee Grinds"/>
+                <GenericInput text="" placeholder="100.22 kg"/>
+                <Dropdown>
+                    <option>N. Van Processor</option>
+                </Dropdown>
+                <IconButton sx={{ "&:hover": { color: "#F86E6E" } }}>
+                    <Delete fontSize="small"/>
                 </IconButton>
             </RowCont>
             <IconButton>
-                <AddCircle  sx={{ "&:hover": { color: "#60C993" } }}/>
+                <AddCircle 
+                    fontSize="small" 
+                    sx={{ "&:hover": { color: "#60C993" } }}
+                />
             </IconButton>
-            <Buttons>
-                <Button borderweight="solid 1px darkgray" textweight="regular" buttoncolor="#D2D1D1" textcolor="black" buttontext="Save Edits"/>
-            </Buttons>
+                <Button 
+                    borderweight="solid 1px darkgray" 
+                    textcolor="black"
+                    textweight="regular" 
+                    buttoncolor="#D2D1D1" 
+                    buttontext="Save Edits" 
+                    hoverbg="#598B2C"
+                />
         </Cont>
     );
 } 
