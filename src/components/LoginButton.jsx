01dd3c58f138810e005df8c400458c0fd18fcdc9
Catherine Lam <catherinelamyw@gmail.com>
2022-04-08 13:45:56 -0700

    styled the login and logout buttons

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+
+const LoginAnchor = styled.a`
+  background-color: #c4c4c4;
+  border-radius: 7px;
+  margin: 5px;
+  padding: 4px 20px 6px 20px;
+  font-size: 14px;
+  color: #000000;
+  text-decoration: none;
+  &:hover {
+    background-color: #ffffff;
+  }
+`;
+
 const LoginButton = () => {
-  return <a href="/login">Log In</a>;
+  return <LoginAnchor href="/login">Log In</LoginAnchor>;
 };
 
 export default LoginButton;
