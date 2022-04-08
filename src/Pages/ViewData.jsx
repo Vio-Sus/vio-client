c58d9dee22adf2dd024b1412cfea46d7fa8fb3e5
jurgenalbanel <72041851+jurgenalbanel@users.noreply.github.com>
2022-04-07 17:27:07 -0700

    alicia changes

--- src/Pages/ViewData.jsx
+++ src/Pages/ViewData.jsx
@@ -4,6 +4,7 @@ import EditForm from '../components/Entry/EditEntryForm';
 import { useEffect, useState } from 'react';
 import Summary from '../components/Summary/Summary';
 import { dateToYMD } from '../common/date';
+import styled from 'styled-components';
 
 const ViewDataPage = ({ sources, items }) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -53,7 +54,29 @@ const ViewDataPage = ({ sources, items }) => {
     startDate &&
     endDate && (
       <>
-        <h1>View Data</h1>
+      <Page>
+      <HeaderCont>
+        <HeaderTextcont>
+                <Header>Graph View</Header>
+                <Subheader>Here’s an overview of the performance.</Subheader>
+        </HeaderTextcont>
+              <ButtonCont>
+                <EachButtonCont> 
+                  <Button/>
+                </EachButtonCont>
+                <EachButtonCont> 
+                  <Button/>
+                </EachButtonCont>
+                <EachButtonCont> 
+                  <Button/>
+                </EachButtonCont>
+              </ButtonCont>
+      </HeaderCont>
+
+        {/* <TextCont>
+          <Heading>List View</Heading>
+          <SubHead>Here's an overview of the performance</SubHead>
+        </TextCont> */}
         <EntriesList
           selectEntry={selectEntry}
           sources={sources}
@@ -77,9 +100,79 @@ const ViewDataPage = ({ sources, items }) => {
         )}
         <Summary startDate={startDate} endDate={endDate} />
         {/* <Summary startDate={'2022-01-01'} endDate={'2022-03-10'} /> */}
+      </Page>
       </>
     )
   );
 };
 
 export default ViewDataPage;
+
+
+// styled components
+
+const Heading = styled.text`
+  font-size: 36px;
+  font-weight: 300;
+  color: black; 
+`;
+
+const SubHead = styled.text`
+  font-size: 18px;
+  font-weight: 500;
+  color: black; 
+`;
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 80vw;
+  height: 100vh;
+  justify content-
+  background-color:blue;
+`
+
+const HeadCont = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  background-color:blue;
+`
+const TextCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  background-color:green;
+`
+const HeaderCont = styled.div`
+  display:flex;
+  flex-direction: row ;
+  justify-content: space-between;
+  width: 80vw;
+  background-color: red;
+`;
+
+const HeaderTextcont = styled.div`
+  display:flex;
+  flex-direction: column ;
+
+`;
+
+const ButtonCont = styled.div`
+  background-color: green ;
+  display:flex;
+  flex-direction: row;
+`;
+const EachButtonCont = styled.div`
+    margin: .3rem;
+`;
+
+const Header = styled.text`
+  font-size: 24px;
+`;
+const Subheader = styled.text`
+  font-size: 12px;
+  color: #888888;
+`;
