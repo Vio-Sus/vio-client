323a967395bffd7774cd2c9fcccae29bcc87d690
Catherine Lam <catherinelamyw@gmail.com>
2022-04-18 11:03:48 -0700

    added icons and kg suffix

--- src/Pages/NewEntry.jsx
+++ src/Pages/NewEntry.jsx
@@ -1,17 +1,15 @@
-import Form from "../components/Entry/AddEntryForm";
+import Form from '../components/Entry/AddEntryForm';
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 
-
-const MainCont= styled.div`
-display: flex;
-justify-content: space-between;
-margin:10%;
-//background-color: blue;
+const MainCont = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10%;
 `;
 
 const Heading = styled.text`
-display: flex;
+  display: flex;
   font-size: 18px;
   margin-bottom: 5%;
 `;
@@ -19,31 +17,28 @@ display: flex;
 const LeftCont = styled.div`
   margin: 10px;
   width: 50%;
-  //background-color: aliceblue;
 `;
 
 const RightCont = styled.div`
   width: 382px;
   height: 606px;
   border-radius: 10px;
-  background-color: #E9E9E9;
+  background-color: #e9e9e9;
   text-align: center;
+  padding: 20px;
 `;
 
 const NewEntryPage = ({ sources, items }) => {
   return (
     <>
-    
+      <MainCont>
+        <LeftCont>
+          <Heading>Input New Entry</Heading>
+          <Form sources={sources} items={items}></Form>
+        </LeftCont>
 
-    <MainCont>
-      <LeftCont>
-        <Heading>Input New Entry</Heading>
-        <Form sources={sources} items={items}></Form>
-      </LeftCont>
-
-      <RightCont>More info here</RightCont>
-
-    </MainCont>
+        <RightCont>More info here</RightCont>
+      </MainCont>
     </>
   );
 };
