ec49bf05b33d145cc61a7498e77a8ed127ee201b
Catherine Lam <catherinelamyw@gmail.com>
2022-04-19 17:26:45 -0700

    started styling items page

--- src/Pages/ViewItem.jsx
+++ src/Pages/ViewItem.jsx
@@ -3,6 +3,7 @@ import ItemsList from '../components/Item/ItemsList';
 import EditItemForm from '../components/Item/EditItemForm';
 // import AddItemForm from '../components/Item/AddItemForm';
 import AddItemModal from '../components/Item/AddItemModal';
+import Button from '../components/Button';
 
 const ViewItemPage = ({ items }) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -25,7 +26,7 @@ const ViewItemPage = ({ items }) => {
   return (
     <>
       <h1>View Items</h1>
-      <button onClick={addItem}>Add New Item</button>
+      <Button onClick={addItem}>Add New Item</Button>
       <ItemsList selectItem={selectItem}></ItemsList>
       {isEditing && (
         <EditItemForm
