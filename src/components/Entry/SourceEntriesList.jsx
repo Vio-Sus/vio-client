1db400bf6ed998fc08885e5ee863113fa80331fd
Adrian Sieroslawski <adrian.sieroslawski@gmail.com>
2022-05-03 10:05:08 -0700

    fixed number display

--- src/components/Entry/SourceEntriesList.jsx
+++ src/components/Entry/SourceEntriesList.jsx
@@ -258,7 +258,7 @@ export default function SourceEntriesList({ collectors, items }) {
               ? totals.map((entry, index) => (
                   <tr key={index}>                                   
                     <td> {entry.item_name} </td>                  
-                    <td> {entry.entry_weight} kg </td>                                    
+                    <td> {entry.entry_weight.toFixed(2)} kg </td>                                    
                   </tr>
                 ))
               : null}              
