443e7c78335f61efff2b0b4de19c32ad1b2e910f
Jo Jo Lam <jojilam@gmail.com>
2022-05-06 19:47:32 -0700

    fixed edit and delete entry

--- src/components/Entry/DeleteEntryConfirmation.jsx
+++ src/components/Entry/DeleteEntryConfirmation.jsx
@@ -16,10 +16,10 @@ export default function DeleteConfirmation({
   // const [date, setDate] = useState('');
   // const [weight, setWeight] = useState(0);
 
-  const [itemId, setItemId] = useState('');
-  const [sourceId, setSourceId] = useState('');
-  const [date, setDate] = useState('');
-  const [weight, setWeight] = useState(0);
+  // const [itemId, setItemId] = useState('');
+  // const [sourceId, setSourceId] = useState('');
+  // const [date, setDate] = useState('');
+  // const [weight, setWeight] = useState(0);
 
   //   useEffect(() => {
   //   (async () => {
@@ -36,16 +36,16 @@ export default function DeleteConfirmation({
   //   })();
   // }, [id]);
 
-  useEffect(() => {
-    if (!entry) {
-      return;
-    }
-    console.log({ entry });
-    setItemId(entry.item_id);
-    setSourceId(entry.source_id);
-    setDate(entry.entry_date);
-    setWeight(entry.entry_weight);
-  }, [entry]);
+  // useEffect(() => {
+  //   if (!entry) {
+  //     return;
+  //   }
+  //   console.log({ entry });
+  //   setItemId(entry.item_id);
+  //   setSourceId(entry.source_id);
+  //   setDate(entry.entry_date);
+  //   setWeight(entry.entry_weight);
+  // }, [entry]);
 
   const handleDelete = () => {
     deleteEntry(entry.entry_id).then((res) => {
@@ -61,9 +61,9 @@ export default function DeleteConfirmation({
 
   return (
     <div class="modal">
-    <div class="modalContent">
-      <div class="modalClose">
-      <IconButton
+      <div class="modalContent">
+        <div class="modalClose">
+          <IconButton
             onClick={handleCancel}
             sx={{
               '&:hover': {
@@ -74,17 +74,17 @@ export default function DeleteConfirmation({
           >
             <CancelIcon />
           </IconButton>
-      </div>
-      <h2>Are you sure you want to delete the following entry?</h2>
-            <p>Sub Account: {sourceId}</p>
-            <p>Materials: {itemId}</p>
-            <p>Date: {date}</p>
-            <p>Weight: {weight} kg</p>
-          <div class="buttonCont">
-            <button onClick={handleDelete} class="submitButton red">
-              Delete Entry
-            </button>
-          </div>
+        </div>
+        <h2>Are you sure you want to delete the following entry?</h2>
+        <p>Source: {entry.source_name}</p>
+        <p>Item: {entry.item_name}</p>
+        <p>Date: {entry.entry_date}</p>
+        <p>Weight: {entry.entry_weight} kg</p>
+        <div class="buttonCont">
+          <button onClick={handleDelete} class="submitButton red">
+            Delete Entry
+          </button>
+        </div>
       </div>
     </div>
   );
